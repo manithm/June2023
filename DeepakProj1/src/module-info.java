d4bf19f4d335ab1d90166ec5298f5ce7a588c8ef
manith mathews <mathewsmanith@gmail.com>
2023-07-02 23:02:48 +1000

    commit 1 from Eclipse(unstage to stage then Commit & push)

---
+++ DeepakProj1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author manit
+ *
+ */
+module DeepakProj1 {
+}
---
+++ DeepakProj1/src/pkg1/Example1.java
@@ -0,0 +1,16 @@
+package pkg1;
+
+public class Example1 {
+	public Example1()
+	{
+		System.out.println("Constructor class");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Main class");
+		
+		 Example1 ex1=new Example1();
+		
+	}
+
+}
